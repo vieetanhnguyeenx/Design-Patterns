f07df6c5b575db7ee1bc5f6e400db94322c5c061
NVA <vieetanhnguyeenx@gmail.com>
2024-04-04 17:17:42 +0700

    Modify Employee.java

--- Strategy/src/Employee.java
+++ Strategy/src/Employee.java
@@ -1,6 +1,6 @@
 public class Employee {
     public static void main(String[] args) {
-        System.out.println("Doi den 1 World!");
+        System.out.println("Doi den 111 World!");
         System.out.println("Hello World!");
 
     }
